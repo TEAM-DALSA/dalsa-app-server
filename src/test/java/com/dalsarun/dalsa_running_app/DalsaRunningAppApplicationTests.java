6ba30e195b66018c8f41ec9b1ab548537dcfdbdc
DongchannN <ldc991104@gmail.com>
2024-09-25 14:16:23 +0900

    first commit: Init spring project
    spring boot version : 3.3.4
    java version        : 17
    dependencies        : Spring Web, Spring Data JPA, Lombok, MySQL Driver

---
+++ src/test/java/com/dalsarun/dalsa_running_app/DalsaRunningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalsarun.dalsa_running_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalsaRunningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
